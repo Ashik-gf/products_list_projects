e5c6e62f45632d73ba60dfa14c498f5bd652f300
ashik-coder <ashikhasan998@gmail.com>
2024-11-05 20:58:30 +0600

    uplode after heroSections

--- src/components/Hero/HeroSectios.jsx
+++ src/components/Hero/HeroSectios.jsx
@@ -1,5 +1,34 @@
-import React from 'react'
-
+import React from 'react';
+const images = [
+    {
+        src: "https://images.unsplash.com/photo-1483985988355-763728e1935b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Summer style 1"
+    },
+    {
+        src: "https://images.unsplash.com/photo-1529139574466-a303027c1d8b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Summer style 2"
+    },
+    {
+        src: "https://images.unsplash.com/photo-1507680434567-5739c80be1ac?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Summer style 3"
+    },
+    {
+        src: "https://images.unsplash.com/photo-1540827109409-17f40944f276?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Summer style 4"
+    },
+    {
+        src: "https://images.unsplash.com/photo-1610419993549-7429619cdbd1?q=80&w=1915&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Summer style 5"
+    },
+    {
+        src: "https://images.unsplash.com/photo-1505022610485-0249ba5b3675?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Summer style 6"
+    },
+    {
+        src: "https://images.unsplash.com/photo-1597196526281-fe4861daa915?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Summer style 7"
+    }
+];
 const HeroSectios = () => {
     return (
         <div>
@@ -17,74 +46,61 @@ const HeroSectios = () => {
                             <div className="mt-10">
                                 {/* <!-- Decorative image grid --> */}
 
-                                <a
-                                    href="#"
+                                <a href="#"
                                     className="inline-block rounded-md border border-transparent bg-teal-600 px-8 py-3 text-center font-medium text-white hover:bg-teal-700"
-                                >Shop Collection</a
-                                >
+                                >Shop Collection</a>
                             </div>
-
-                            <div
-                                aria-hidden="true"
-                                className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
-                            >
+                            <div aria-hidden="true"
+                                className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl">
                                 <div
-                                    className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8"
-                                >
+                                    className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
                                     <div className="flex items-center space-x-6 lg:space-x-8">
                                         <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                                             <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
                                                 <img
-                                                    src="https://images.unsplash.com/photo-1483985988355-763728e1935b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                                    alt=""
-                                                    className="h-full w-full object-cover object-center"
-                                                />
+                                                    src={images[0].src}
+                                                    alt={images[0].alt}
+                                                    className="h-full w-full object-cover object-center" />
                                             </div>
                                             <div className="h-64 w-44 overflow-hidden rounded-lg">
                                                 <img
-                                                    src="https://images.unsplash.com/photo-1529139574466-a303027c1d8b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                                    alt=""
-                                                    className="h-full w-full object-cover object-center"
-                                                />
+                                                    src={images[1].src}
+                                                    alt={images[1].alt}
+                                                    className="h-full w-full object-cover object-center" />
                                             </div>
                                         </div>
                                         <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                                             <div className="h-64 w-44 overflow-hidden rounded-lg">
                                                 <img
-                                                    src="https://images.unsplash.com/photo-1507680434567-5739c80be1ac?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                                    alt=""
-                                                    className="h-full w-full object-cover object-center"
-                                                />
+                                                    src={images[2].src}
+                                                    alt={images[2].alt}
+                                                    className="h-full w-full object-cover object-center" />
                                             </div>
                                             <div className="h-64 w-44 overflow-hidden rounded-lg">
                                                 <img
-                                                    src="https://images.unsplash.com/photo-1540827109409-17f40944f276?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                                    alt=""
-                                                    className="h-full w-full object-cover object-center"
-                                                />
+                                                    src={images[3].src}
+                                                    alt={images[3].alt}
+                                                    className="h-full w-full object-cover object-center" />
                                             </div>
                                             <div className="h-64 w-44 overflow-hidden rounded-lg">
                                                 <img
-                                                    src="https://images.unsplash.com/photo-1610419993549-7429619cdbd1?q=80&w=1915&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                                    alt=""
-                                                    className="h-full w-full object-cover object-center"
-                                                />
+                                                    src={images[4].src}
+                                                    alt={images[4].alt}
+                                                    className="h-full w-full object-cover object-center" />
                                             </div>
                                         </div>
                                         <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                                             <div className="h-64 w-44 overflow-hidden rounded-lg">
                                                 <img
-                                                    src="https://images.unsplash.com/photo-1505022610485-0249ba5b3675?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                                    alt=""
-                                                    className="h-full w-full object-cover object-center"
-                                                />
+                                                    src={images[5].src}
+                                                    alt={images[5].alt}
+                                                    className="h-full w-full object-cover object-center" />
                                             </div>
                                             <div className="h-64 w-44 overflow-hidden rounded-lg">
                                                 <img
-                                                    src="https://images.unsplash.com/photo-1597196526281-fe4861daa915?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                                    alt=""
-                                                    className="h-full w-full object-cover object-center"
-                                                />
+                                                    src={images[6].src}
+                                                    alt={images[6].alt}
+                                                    className="h-full w-full object-cover object-center" />
                                             </div>
                                         </div>
                                     </div>
